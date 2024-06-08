506e55d55cdf3bf59187cde51e361298e2e592cc
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2024-06-08 15:49:57 -0500

    Created Fighter everything

--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -10,4 +10,55 @@ const router = Router();
 
 // TODO: Implement route controllers for fighter
 
+// GET route for retrieving all fighters
+router.get("/", async (req, res, next) => {
+  try {
+    const fighters = await fighterService.getAllFighters();
+    res.json(fighters);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// POST route for creating a new fighter
+router.post("/", createFighterValid, async (req, res, next) => {
+  try {
+    const fighter = await fighterService.createFighter(req.body);
+    res.json(fighter);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// GET route for retrieving a fighter by ID
+router.get("/:id", async (req, res, next) => {
+  try {
+    const fighter = await fighterService.getFighterById(req.params.id);
+    res.json(fighter);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// PATCH route for updating a fighter
+router.patch("/:id", updateFighterValid, async (req, res, next) => {
+  try {
+    const fighter = await fighterService.updateFighter(req.params.id, req.body);
+    res.json(fighter);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// DELETE route for deleting a fighter
+router.delete("/:id", async (req, res, next) => {
+  try {
+    await fighterService.deleteFighter(req.params.id);
+    res.json({ message: "Fighter deleted successfully" });
+  } catch (err) {
+    next(err);
+  }
+});
+
+
 export { router };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,6 +11,54 @@ const router = Router();
 // TODO: Implement route controllers for user
 
 
+// GET route for retrieving all users
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await userService.getAllUsers();
+    res.json(users);
+  } catch (err) {
+    next(err);
+  }
+});
 
+// POST route for creating a new user
+router.post('/', createUserValid, async (req, res, next) => {
+  try {
+    const user = await userService.createUser(req.body);
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// GET route for retrieving a user by ID
+router.get('/:id', async (req, res, next) => {
+  try {
+    const user = await userService.getUserById(req.params.id);
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// PATCH route for updating a user
+router.patch('/:id', updateUserValid, async (req, res, next) => {
+  try {
+    const user = await userService.updateUser(req.params.id, req.body);
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// DELETE route for deleting a user
+router.delete('/:id', async (req, res, next) => {
+  try {
+    await userService.deleteUser(req.params.id);
+    res.json({ message: 'User deleted successfully' });
+  } catch (err) {
+    next(err);
+  }
+});
 
 export { router };
