ba34f81afc395dab4f682496a2725068fcfee191
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2024-06-09 15:51:18 -0500

    document

--- middlewares/fighter.validation.middleware.js
+++ middlewares/fighter.validation.middleware.js
@@ -1,14 +1,31 @@
 import { FIGHTER } from "../models/fighter.js";
 import { fighterRepository } from "../repositories/fighterRepository.js";
 
+
+/**
+ * Middleware function for validating a fighter entity during creation.
+ * 
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ * 
+ * @property {string} req.body.name - The name of the fighter. It is converted to lowercase.
+ * @property {number} req.body.power - The power of the fighter. It must be a number between 1 and 100.
+ * @property {number} req.body.defense - The defense of the fighter. It must be a number between 1 and 100.
+ * 
+ * @throws {string} "Content can not be empty!" - If the request body is empty.
+ * @throws {string} "Required fields are missing!" - If the request body does not have all the required fields (name, power, defense).
+ * @throws {string} "Fighter with the same name already exists!" - If a fighter with the same name already exists.
+ * @throws {string} "Power must be a number between 1 and 100!" - If power is not a number between 1 and 100.
+ * @throws {string} "Defense must be a number between 1 and 100!" - If defense is not a number between 1 and 100.
+ */
 const createFighterValid = (req, res, next) => {
-  // TODO: Implement validatior for FIGHTER entity during creation
+  // DONE: Implement validatior for FIGHTER entity during creation 
 
   // Convert the name to lowercase
   if (req.body.name) {
     req.body.name = req.body.name.toLowerCase();
   }
-
 
   // Check if the request body is empty
   if (!req.body) {
@@ -25,7 +42,7 @@ const createFighterValid = (req, res, next) => {
     req.error_message = "Fighter with the same name already exists!";
   }
   
-  // Checkif power is a number between 1 and 100
+  // Check if power is a number between 1 and 100
   if (req.body.power) {
     let power = parseInt(req.body.power, 10);
     if (power < 1 || req.body.power > 100) {
@@ -33,7 +50,7 @@ const createFighterValid = (req, res, next) => {
     }
   }
 
-  // Checkif defense is a number between 1 and 100
+  // Check if defense is a number between 1 and 100
   if (req.body.defense) {
     if (req.body.defense < 1 || req.body.defense > 100) {
       req.error_message = "Defense must be a number between 1 and 100!";
@@ -44,27 +61,41 @@ const createFighterValid = (req, res, next) => {
 
 
 
-
+/**
+ * Middleware function for validating a fighter entity during update.
+ * 
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ * 
+ * @property {Object} req.body - The body of the request. It should not be empty.
+ * @property {string} req.params.id - The ID of the fighter to update.
+ * 
+ * @throws {string} "Content can not be empty!" - If the request body is empty.
+ * @throws {string} "Fighter not found!" - If the fighter with the provided ID does not exist.
+ */
 const updateFighterValid = (req, res, next) => {
-  // TODO: Implement validatior for FIGHTER entity during update DONE!
+  // DONE: Implement validatior for FIGHTER entity during update DONE!
 
   // Check if the request body is empty
   if (!req.body) {
     req.error_message = "Content can not be empty!";
-    
-   
   }
   // Check if the requested fighter exists
-const fighter = fighterRepository.getOne({ id: req.params.id });
+  const fighter = fighterRepository.getOne({ id: req.params.id });
   if (!fighter) {
     req.error_message = "Fighter not found!";
   }
-
-
   next();
 };
 
-
+/**
+ * Function to validate if a fighter's name is unique.
+ * 
+ * @param {string} name - The name of the fighter.
+ * 
+ * @returns {boolean} - Returns false if a fighter with the same name exists, true otherwise.
+ */
 function isNameValid(name) {
   
   var fighter = fighterRepository.getOne({ name });
--- middlewares/user.validation.middleware.js
+++ middlewares/user.validation.middleware.js
@@ -1,29 +1,53 @@
 import { USER } from "../models/user.js";
 import { userRepository } from "../repositories/userRepository.js";
+
+
+/**
+ * Middleware function for validating a user entity during creation.
+ * 
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ * 
+ * @property {string} req.body.email - The email of the user. It is converted to lowercase and must be a valid Gmail address.
+ * @property {string} req.body.password - The password of the user. It must be at least 3 characters long.
+ * @property {string} req.body.firstName - The first name of the user.
+ * @property {string} req.body.lastName - The last name of the user.
+ * @property {string} req.body.phoneNumber - The phone number of the user. It must start with +380 and be 13 characters long.
+ * 
+ * @throws {string} "Content can not be empty!" - If the request body is empty.
+ * @throws {string} "Required fields are missing!" - If the request body does not have all the required fields (email, password, firstName, lastName, phoneNumber).
+ * @throws {string} "Invalid email!" - If the email is not a valid Gmail address.
+ * @throws {string} "Password must be at least 3 characters!" - If the password is less than 3 characters long.
+ * @throws {string} "Phone number must have 13 characters and start with +380!" - If the phone number does not start with +380 or is not 13 characters long.
+ * @throws {string} "User with the same email already exists!" - If a user with the same email already exists.
+ * @throws {string} "User with the same phone number already exists!" - If a user with the same phone number already exists.
+ */
+
 const createUserValid = (req, res, next) => {
-  // TODO: Implement validatior for USER entity during creation
+  // DONE: Implement validatior for USER entity during creation
 
   // Convert the email to lowercase
   if(req.body.email )
     {
       req.body.email = req.body.email.toLowerCase();
     }
+
   // Check if the request body is empty
   if (!req.body) {
-
     req.error_message = "Content can not be empty!"
-    
   }
+
   // Check if the request body has all the required fields
   if (!req.body.email || !req.body.password || !req.body.firstName || !req.body.lastName || !req.body.phoneNumber ) {
    req.error_message = "Required fields are missing!"
-   
   }
+
   // Check if the email is valid
   if (req.body.email && !req.body.email.includes("@gmail.com")) {
     req.error_message = "Invalid email!"
-    
   }
+
   // Check if the password is valid
   if (req.body.password) {
     if (req.body.password.length < 3)
@@ -31,14 +55,15 @@ const createUserValid = (req, res, next) => {
         req.error_message = "Password must be at least 3 characters!"
       }
   }
+
   // CHeck if the phone number starts with +380 and has 13 characters
   if (req.body.phoneNumber ) {
     if (!req.body.phoneNumber.startsWith("+380") || req.body.phoneNumber.length !== 13) {
       req.error_message = "Phone number must have 13 characters and start with +380!"
     }
   }
-  // Check if the user with the same email already exists
 
+  // Check if the user with the same email already exists
   if (!isEmailValid(req.body.email)) {
     req.error_message = "User with the same email already exists!"
   }
@@ -51,14 +76,28 @@ const createUserValid = (req, res, next) => {
   next();
 };
 
+
+/**
+ * Middleware function for validating a user entity during update.
+ * 
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ * 
+ * @property {Object} req.body - The body of the request. It should not be empty.
+ * @property {string} req.params.id - The ID of the user to update.
+ * 
+ * @throws {string} "Content can not be empty!" - If the request body is empty.
+ * @throws {string} "User not found!" - If the user with the provided ID does not exist.
+ */
 const updateUserValid = (req, res, next) => {
-  // TODO: Implement validatior for user entity during update DONE!
+  // DONE: Implement validatior for user entity during update DONE!
 
   // Check if the request body is empty
   if (!req.body) {
     req.error_message = "Content can not be empty!";
-
   }
+
   // Check if the requested user exists
   const user = userRepository.getOne({ id: req.params.id });
   if (!user) {
@@ -67,8 +106,15 @@ const updateUserValid = (req, res, next) => {
   next();
 };
 
-function isEmailValid(email) {
 
+/**
+ * Function to validate if a user's email is unique.
+ * 
+ * @param {string} email - The email of the user.
+ * 
+ * @returns {boolean} - Returns false if a user with the same email exists, true otherwise.
+ */
+function isEmailValid(email) {
 var user = userRepository.getOne({ email });
 if (user) {
   return false;
@@ -76,9 +122,16 @@ if (user) {
 else {
   return true;
 }
-
 }
 
+
+/**
+ * Function to validate if a user's phone number is unique.
+ * 
+ * @param {string} phoneNumber - The phone number of the user.
+ * 
+ * @returns {boolean} - Returns false if a user with the same phone number exists, true otherwise.
+ */
 function isPhoneNumberValid(phoneNumber) {
   var user = userRepository.getOne({ phoneNumber });
   if (user) {
